81aaf7599a55122b1505105aa83381c14b1659e2
FrancoSMiño <frankow222@gmail.com>
2023-10-17 10:46:08 -0300

    feat!: reamake with express BREAKING CHANGE: modified endpoints

--- server/index.js
+++ server/index.js
@@ -1,101 +1,93 @@
-const http = require('http');
+const express = require('express');
 const { query } = require('./database.js');
 
 const PORT = 5000;
 
-const server = http.createServer();
+const app = express();
 
-server.on('request', async (req, res) => {
-  const { method, url, headers } = req;
-  const parsedURL = new URL(url, `http://${headers.host}`);
+//Get all posts
+app.get('/', async (req, res) => {
+  const { rows } = await query('SELECT * FROM posts');
 
-  if (
-    method === 'GET' &&
-    parsedURL.pathname === '/posts' &&
-    !parsedURL.searchParams.get('id')
-  ) {
-    const clientQuery = await query('SELECT * FROM posts');
+  res.writeHead(200, { 'Content-type': 'application/json' });
+  res.end(JSON.stringify(rows));
+});
 
+app.listen(PORT, () => {
+  console.log(`server running on port: ${PORT}`);
+});
+
+//Get post using post_id
+app.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { rows } = await query('SELECT * FROM posts WHERE post_id=$1', [id]);
+  const post = rows.length === 1;
+
+  if (post) {
     res.writeHead(200, { 'Content-type': 'application/json' });
-    res.end(JSON.stringify(clientQuery.rows));
-  } else if (
-    method === 'GET' &&
-    parsedURL.pathname === '/posts' &&
-    parsedURL.searchParams.get('id')
-  ) {
-    const id = Number(parsedURL.searchParams.get('id'));
-    const clientQuery = await query('SELECT * FROM posts WHERE post_id=$1', [
-      id,
-    ]);
-    const post = clientQuery.rows.length === 1;
-
-    if (post) {
-      res.writeHead(200, { 'Content-type': 'application/json' });
-      res.end(JSON.stringify(clientQuery.rows[0]));
-    } else {
-      res.writeHead(404, { 'Content-type': 'text/plain' });
-      res.end('Post not found or id invalid');
-    }
-  } else if (method === 'POST' && parsedURL.pathname === '/posts') {
-    let body = '';
-
-    req.on('data', chunk => {
-      body += chunk;
-    });
-
-    req.on('end', async () => {
-      const newPost = JSON.parse(body);
-
-      if (newPost.content && newPost.username) {
-        const clientQuery = await query(
-          'INSERT INTO posts(post_author, post_body) VALUES($1, $2) RETURNING *',
-          [newPost.username, newPost.content]
-        );
-
-        res.writeHead(201, { 'Content-Type': 'text/plain' });
-        res.end(JSON.stringify(clientQuery.rows[0]));
-      } else {
-        res.writeHead(400, 'Bad Request');
-        res.end('There is an error on the request syntax');
-      }
-    });
-  } else if (
-    method === 'PUT' &&
-    parsedURL.pathname === '/posts' &&
-    parsedURL.searchParams.get('id')
-  ) {
-    const id = Number(parsedURL.searchParams.get('id'));
-    let body = '';
-
-    req.on('data', chunk => {
-      body += chunk;
-    });
-
-    req.on('end', async () => {
-      const clientQuery = await query(
-        'UPDATE posts SET post_body=$1 WHERE post_id=$2 RETURNING *',
-        [body, id]
-      );
-
-      res.end(JSON.stringify(clientQuery.rows[0]));
-    });
-  } else if (
-    method === 'DELETE' &&
-    parsedURL.pathname === '/posts' &&
-    parsedURL.searchParams.get('id')
-  ) {
-    const id = Number(parsedURL.searchParams.get('id'));
-
-    await query('DELETE FROM posts WHERE post_id=$1', [id]);
-
-    res.writeHead(204, { 'Content-Type': 'application/json' });
-    res.end();
+    res.end(JSON.stringify(rows[0]));
   } else {
-    res.writeHead(404, 'text/plain');
-    res.end('Error: Not Found');
+    res.writeHead(404, { 'Content-type': 'text/plain' });
+    res.end('Post not found or id invalid');
   }
 });
 
-server.listen(PORT, () => {
-  console.log(`server running on port: ${PORT}`);
+//Create a post
+app.post('/submit', (req, res) => {
+  let body = '';
+
+  req.on('data', chunk => {
+    body += chunk;
+  });
+
+  req.on('end', async () => {
+    const newPost = JSON.parse(body);
+
+    if (newPost.content && newPost.username) {
+      const { rows } = await query(
+        'INSERT INTO posts(post_author, post_body) VALUES($1, $2) RETURNING *',
+        [newPost.username, newPost.content]
+      );
+
+      res.writeHead(201, { 'Content-Type': 'text/plain' });
+      res.end(JSON.stringify(rows[0]));
+    } else {
+      res.writeHead(400, 'Bad Request');
+      res.end('There is an error on the request syntax');
+    }
+  });
+});
+
+//Update a post using post_id
+app.put('/:id', (req, res) => {
+  const { id } = req.params;
+  let body = '';
+
+  req.on('data', chunk => {
+    body += chunk;
+  });
+
+  req.on('end', async () => {
+    const { rows } = await query(
+      'UPDATE posts SET post_body=$1 WHERE post_id=$2 RETURNING *',
+      [body, id]
+    );
+
+    res.end(JSON.stringify(rows[0]));
+  });
+});
+
+//Delete a post using post_id
+app.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  await query('DELETE FROM posts WHERE post_id=$1', [id]);
+
+  res.writeHead(204, { 'Content-Type': 'application/json' });
+  res.end();
+});
+
+//Not Found
+app.use((req, res) => {
+  res.status(404).send('Not Found');
 });
